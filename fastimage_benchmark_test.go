b7856d89885cfd4abab3fe3c243eec4b0a341215
Niko Bellic <niko.bellic@kakaocorp.com>
2017-01-06 17:29:03 +0900

    added sample file

--- fastimage_benchmark_test.go
+++ fastimage_benchmark_test.go
@@ -1,6 +1,10 @@
 package fastimage
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 // func BenchmarkSample(b *testing.B) {
 // 	for i := 0; i < b.N; i++ {
@@ -11,12 +15,27 @@ import "testing"
 // }
 
 func BenchmarkCustomTimeout(b *testing.B) {
-	// url := "http://upload.wikimedia.org/wikipedia/commons/9/9a/SKA_dishes_big.jpg"
-	url := "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"
-	// url := "http://loremflickr.com/500/500"
-
-	for i := 0; i < b.N; i++ {
-		it, is, err := DetectImageTypeWithTimeout(url, 10000)
-		b.Logf("type:%v, size:%v, err:%v", it, is, err)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	images, err := readSampleFile("imgsrc.log")
+	if err != nil {
+		b.Errorf("failed to open image source file : %v", err)
 	}
+
+	type counter struct {
+		success int
+		failure int
+	}
+	c := counter{}
+	for i := 0; i < b.N; i++ {
+		url := images[r.Intn(len(images))]
+		_, _, err := DetectImageTypeWithTimeout(url, 1000)
+		// it, is, err := DetectImageTypeWithTimeout(url, 10000)
+		// b.Logf("type:%v, size:%v, err:%v", it, is, err)
+		if err == nil {
+			c.success++
+		} else {
+			c.failure++
+		}
+	}
+	b.Logf("success:%v, failure:%v", c.success, c.failure)
 }
