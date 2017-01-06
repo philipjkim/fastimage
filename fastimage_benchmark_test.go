84938f2fedc01ab5af717eaf35f758cead719269
Niko Bellic <niko.bellic@kakaocorp.com>
2017-01-06 17:45:45 +0900

    added file logging due to output truncate

--- fastimage_benchmark_test.go
+++ fastimage_benchmark_test.go
@@ -1,7 +1,9 @@
 package fastimage
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
 	"testing"
 	"time"
 )
@@ -13,26 +15,26 @@ func BenchmarkCustomTimeout(b *testing.B) {
 		b.Errorf("failed to open image source file : %v", err)
 	}
 
+	logfile, err := os.Create("imgfetch.log")
+	defer logfile.Close()
+	if err != nil {
+		b.Errorf("failed to create log file : %v", err)
+	}
 	type counter struct {
 		success int
 		failure int
 	}
 	c := counter{}
 	for i := 0; i < b.N; i++ {
-<<<<<<< HEAD
 		url := images[r.Intn(len(images))]
-		_, _, err := DetectImageTypeWithTimeout(url, 1000)
-		// it, is, err := DetectImageTypeWithTimeout(url, 10000)
-		// b.Logf("type:%v, size:%v, err:%v", it, is, err)
+		// _, _, err := DetectImageTypeWithTimeout(url, 1000)
+		it, is, err := DetectImageTypeWithTimeout(url, 1000)
+		logfile.WriteString(fmt.Sprintf("url:%v, type:%v, size:%v, err:%v\n", url, it, is, err))
 		if err == nil {
 			c.success++
 		} else {
 			c.failure++
 		}
-=======
-		it, is, err := DetectImageTypeWithTimeout(url, 1000)
-		b.Logf("type:%v, size:%v, err:%v", it, is, err)
->>>>>>> e1a1877e509a0bcf0acc307d7349ee49da3c54be
 	}
 	b.Logf("success:%v, failure:%v", c.success, c.failure)
 }
